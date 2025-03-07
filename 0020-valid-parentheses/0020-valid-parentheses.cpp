fb8e01f5792d528fb559810054da86f2b8f2e541
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-03-07 01:35:15 +0100

    Time: 0 ms (100%), Space: 8.6 MB (83.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,14 +3,14 @@ public:
     bool isValid(string s) {
         vector<char> stack;
         for (const char &c : s) {
-            if ("({["sv.contains(c)) {
-                stack.push_back(c);
-            } else {
-                const unordered_map<char, char> mappings = {{')', '('}, {'}', '{'}, {']', '['}};
-                if (stack.empty() || stack.back() != mappings.at(c)) {
+            if (c == ')' || c == '}' || c == ']') {
+                if (stack.empty()
+                    || stack.back() != (c == ')' ? '(' : c == '}' ? '{' : '[')) {
                     return false;
                 }
                 stack.pop_back();
+            } else {
+                stack.push_back(c);
             }
         }
         return stack.empty();
