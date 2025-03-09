8249d9544b867423bc465cb843764ea9012f43c2
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-03-09 23:51:10 +0100

    Time: 0 ms (100%), Space: 71.7 MB (80.9%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cur = 0;
+        int m = INT_MIN;
+        for (const int& num : nums) {
+            cur += num;
+            m = max(m, cur);
+            if (cur < 0)
+                cur = 0;
+        }
+        return m;
+    }
+};
