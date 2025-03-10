e62a4ef210283822a6348fe3efed1cfdfb2935b3
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-03-10 01:45:22 +0100

    Time: 0 ms (100%), Space: 22.5 MB (6.47%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>> intervals,
-                               vector<int>& newInterval) {
-        if (intervals.empty()) {
-            return {newInterval};
-        }
-
+                               const vector<int>& newInterval) {
         const int& start_new = newInterval[0];
         const int& end_new = newInterval[1];
         for (auto i = intervals.begin(); i != intervals.end(); ++i) {
             int& start_i = (*i)[0];
             int& end_i = (*i)[1];
             if ((start_i <= start_new && start_new <= end_i) ||
-                start_new <= start_i && start_i <= end_new) {
-                if (start_new < start_i) {
-                    start_i = start_new;
-                }
+                (start_new <= start_i && start_i <= end_new)) {
+                start_i = min(start_i, start_new);
                 if (end_new > end_i) {
                     end_i = end_new;
                     auto j = i + 1;
@@ -25,10 +19,10 @@ public:
                     }
                     intervals.erase(i + 1, j);
                 }
-
                 return intervals;
-            } else if (end_new < start_i &&
-                       (i == intervals.begin() || start_new > (*(i - 1))[1])) {
+            } else if (const auto prev = i - 1;
+                       end_new < start_i &&
+                       (i == intervals.begin() || start_new > (*prev)[1])) {
                 intervals.insert(i, newInterval);
                 return intervals;
             }
