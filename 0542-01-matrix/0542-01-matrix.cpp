fdd84d3c9f844b2427e62fcdfc5d80d990418084
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-03-11 00:15:35 +0100

    Time: 12 ms (81.8%), Space: 34.7 MB (75.5%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,33 +3,29 @@ public:
     vector<vector<int>> updateMatrix(const vector<vector<int>>& mat) {
         const size_t max_y = mat.size();
         const size_t max_x = mat[0].size();
-        deque<tuple<int, int, int>> stack;
+        queue<pair<int, int>> stack;
         vector<vector<int>> result(max_y, vector(max_x, 100'000));
         for (size_t y = 0; y < max_y; ++y) {
             for (size_t x = 0; x < max_x; ++x) {
                 if (mat[y][x] == 0) {
-                    stack.emplace_back(y, x, 0);
+                    stack.emplace(y, x);
+                    result[y][x] = 0;
                 }
             }
         }
 
         while (!stack.empty()) {
-            const auto [y, x, dist] = stack.front();
-            stack.pop_front();
-
-            if (result[y][x] <= dist) {
-                continue;
-            }
-            result[y][x] = dist;
+            const auto [y, x] = stack.front();
+            stack.pop();
 
             for (const auto& [dy, dx] :
                  {pair{0, 1}, {0, -1}, {1, 0}, {-1, 0}}) {
                 const int ny = y + dy;
                 const int nx = x + dx;
-                const auto np = pair{ny, nx};
                 if (ny >= 0 && ny < max_y && nx >= 0 && nx < max_x &&
-                    mat[ny][nx] != 0 && result[ny][nx] > dist + 1) {
-                    stack.emplace_back(ny, nx, dist + 1);
+                    mat[ny][nx] != 0 && result[ny][nx] > result[y][x] + 1) {
+                    result[ny][nx] = result[y][x] + 1;
+                    stack.emplace(ny, nx);
                 }
             }
         }
