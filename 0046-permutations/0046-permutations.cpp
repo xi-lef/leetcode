ba151edbd4b2df1dd6b0b1297578bfa097285f74
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-03-19 00:45:03 +0100

    Time: 0 ms (100%), Space: 10.8 MB (41.86%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+#if 0
         ranges::sort(nums);
         vector<vector<int>> answer{};
         do {
             answer.push_back(nums);
         } while (ranges::next_permutation(nums).found);
         return answer;
+#else
+        set<vector<int>> answer{nums};
+        bla(answer, nums);
+        return {answer.begin(), answer.end()};
+#endif
+    }
+
+    void bla(set<vector<int>>& answer, vector<int>& cur, int n = 0) {
+        for (int i = n + 1; i < cur.size(); ++i) {
+            bla(answer, cur, n + 1);
+            swap(cur[n], cur[i]);
+            answer.insert(cur);
+            bla(answer, cur, n + 1);
+            swap(cur[n], cur[i]);
+        }
     }
 };
