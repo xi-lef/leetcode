a48f50e66d0c2dd91f3f1f942cad7772f3b84fee
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-04-02 12:22:42 +0200

    Time: 0 ms (100%), Space: 11.5 MB (66.82%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char, int> target{};
-        for (const char& c : p)
-            ++target[c];
-
-        unordered_map<char, int> count{};
-        const int p_n = p.size();
-        for (int i = 0; i < p_n; ++i)
-            ++count[s[i]];
-
         const int s_n = s.size();
+        const int p_n = p.size();
+        if (s_n < p_n)
+            return {};
+
+        array<int, 26> target{};
+        for (const char& c : p)
+            ++target[c - 'a'];
+        array<int, 26> count{};
+        for (int i = 0; i < p_n; ++i)
+            ++count[s[i] - 'a'];
+
         vector<int> anagrams{};
         for (int i = 0; i < s_n - p_n + 1; ++i) {
             if (count == target)
                 anagrams.push_back(i);
-            if (--count[s[i]] == 0)
-                count.erase(s[i]);
-            if (i < s_n - p_n)
-                ++count[s[i + p_n]];
+
+            --count[s[i] - 'a'];
+            if (i + p_n < s_n)
+                ++count[s[i + p_n] - 'a'];
         }
         return anagrams;
     }
