a914a16f7112840362ef8d2ebd31851389c10b6b
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-04-14 23:55:24 +0200

    Time: 0 ms (100%), Space: 23.9 MB (50.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+#if 0
         for (int& i : ranges::remove(nums, 0))
             i = 0;
+#else
+        int free = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[free]);
+                ++free;
+            }
+        }
+#endif
     }
 };
