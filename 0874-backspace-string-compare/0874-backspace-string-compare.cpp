a73214e3f54eb7a0979faecdae76942f148c9491
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-04-14 15:16:02 +0200

    Time: 0 ms (100%), Space: 8.4 MB (68.97%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        ssize_t s_i = s.size() - 1;
+        ssize_t t_i = t.size() - 1;
+        for (int s_skip = 0, t_skip = 0;;) {
+            const auto skip = [](string& s, ssize_t& i, int& skip) {
+                if (i >= 0 && (s[i] == '#' || skip > 0)) {
+                    if (s[i] == '#')
+                        ++skip;
+                    else
+                        --skip;
+                    --i;
+                    return true;
+                }
+                return false;
+            };
+            if (skip(s, s_i, s_skip) || skip(t, t_i, t_skip))
+                continue;
+            if (s_i < 0 || t_i < 0)
+                break;
+            if (s[s_i--] != t[t_i--])
+                return false;
+        }
+        return s_i == -1 && t_i == -1;
+    }
+};
