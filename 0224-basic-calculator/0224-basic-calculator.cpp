48e453d02ab3421bc9cb27bb0e07da1d72dccf55
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-06-04 01:55:54 +0200

    Time: 0 ms (100%), Space: 10.6 MB (75.78%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -8,7 +8,6 @@ public:
 private:
     pair<int, int> calc(string_view s) {
         int answer = 0;
-        int depth = 0;
         int num = 0;
         bool prev_digit = false;
         int sign = 1;
@@ -22,16 +21,11 @@ private:
             prev_digit = false;
 
             if (c == ')') {
-                if (--depth < 0)
-                    break;
+                break;
             } else if (c == '(') {
-                if (depth++ > 0)
-                    continue;
                 const auto [res, skip] = calc(s.substr(i + 1));
                 answer += sign * res;
-                i += skip;
-            } else if (depth > 0) {
-                continue;
+                i += skip + 1;
             } else if (c == '-') {
                 sign = -1;
             } else if (c == '+') {
