ec918ecf50224097ef9453e5324d996ca8fb503f
Felix W. <45627088+xi-lef@users.noreply.github.com>
2025-06-20 17:36:25 +0200

    Time: 25 ms (98.79%), Space: 32.2 MB (98.48%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -14,11 +14,14 @@ public:
 
             int dist = abs(x) + abs(y);
             if (x == 0)
-                dist += 2 * min(k, count[y > 0 ? 'S' : 'N'] + min(count['W'], count['E']));
+                dist += 2 * min(k, count[y > 0 ? 'S' : 'N'] +
+                                       min(count['W'], count['E']));
             else if (y == 0)
-                dist += 2 * min(k, min(count['S'], count['N']) + count[x > 0 ? 'W' : 'E']);
+                dist += 2 * min(k, min(count['S'], count['N']) +
+                                       count[x > 0 ? 'W' : 'E']);
             else
-                dist += 2 * min(k, count[x > 0 ? 'W' : 'E'] + count[y > 0 ? 'S' : 'N']);
+                dist += 2 * min(k, count[x > 0 ? 'W' : 'E'] +
+                                       count[y > 0 ? 'S' : 'N']);
             best = max(best, dist);
         }
         return best;
